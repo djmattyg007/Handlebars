73ead1f5a65275482954091982f543738c918453
Matthew Gamble <git@matthewgamble.net>
2016-06-22 18:42:46 +1000

    More stripping of trailing whitespace

--- src/Data.php
+++ src/Data.php
@@ -41,7 +41,7 @@ class Data extends Base
      * correct results
      *
      * @param string $path Dot notated path
-     * @param int $i    Current context
+     * @param int $i Current context
      * @return mixed
      */
     public function find(string $path, int $i = 0)
@@ -56,11 +56,11 @@ class Data extends Base
         if (strpos($path, '../') === 0) {
             return $this->find(substr($path, 3), $i + 1);
         }
-        
+
         if (strpos($path, './') === 0) {
             return $this->find(substr($path, 2), $i);
         }
-        
+
         //separate by .
         $path = explode('.', $path);
         $last = count($path) - 1;
@@ -72,39 +72,39 @@ class Data extends Base
                 if (isset($current[$node])) {
                     return $current[$node];
                 }
-                
+
                 //is it length ?
                 if ($node === 'length') {
                     //is it a string?
                     if (is_string($current)) {
                         return strlen($current);
                     }
-                    
+
                     //is it an array?
                     if (is_array($current) || $current instanceof \Countable) {
                         return count($current);
                     }
-                    
+
                     //we cant count it, so it's 0
                     return 0;
                 }
             }
-            
+
             //we are not at the last node...
-            
+
             //does the node exist and is it an array ?
             if (isset($current[$node]) && is_array($current[$node])) {
                 //great we can continue
                 $current = $current[$node];
                 continue;
             }
-            
+
             //if it exists and we are just getting the length
             if (isset($current[$node]) && $path[$i + 1] === 'length' && ($i + 1) === $last) {
                 //let it continue
                 continue;
             }
-            
+
             //if we are here, then there maybe a node in current,
             //but there's still more nodes to process
             //either way it cannot be what we are searching for
@@ -113,7 +113,7 @@ class Data extends Base
 
         return null;
     }
-    
+
     /**
      * Returns whatever the current context is
      *
@@ -123,7 +123,7 @@ class Data extends Base
     {
         return $this->tree[0];
     }
-    
+
     /**
      * Pushes a new context in the tree
      *
@@ -135,7 +135,7 @@ class Data extends Base
         array_unshift($this->tree, $data);
         return $this;
     }
-    
+
     /**
      * Pops the last context
      *
