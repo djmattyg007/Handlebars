347cdb829c2c0cd394995e2a63194883b39ffd5e
Matthew Gamble <git@matthewgamble.net>
2016-06-24 22:52:32 +1000

    Change each helper to be an invokable class

---
+++ src/Helper/EachHelper.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace Eden\Handlebars\Helper;
+
+class EachHelper
+{
+    public function __invoke($object, $options)
+    {
+        $args = func_get_args();
+        $options = array_pop($args);
+
+        if (is_scalar($object) || !$object) {
+            return $options['inverse']();
+        }
+
+        //test foreach
+        $keyName = null;
+        $valueName = null;
+        //see handlebars.js {{#each array as |value, key|}}
+        if (strpos($options['args'], ' as |') !== false
+            && substr_count($options['args'], '|') === 2
+        ) {
+            list($tmp, $valueName) = explode('|', $options['args']);
+
+            if (strpos($valueName, ',') !== false) {
+                list($valueName, $keyName) = explode(',', trim($valueName));
+            }
+
+            $keyName = trim((string) $keyName);
+            $valueName = trim($valueName);
+        }
+
+        $buffer = array();
+        $object = (array) $object;
+
+        //get last
+        end($object);
+        $last = key($object);
+
+        //get first
+        reset($object);
+        $first = key($object);
+
+        foreach ($object as $key => $value) {
+            if (!is_array($value)) {
+                $value = array('this' => $value);
+            } else {
+                $value['this'] = $value;
+            }
+
+            if ($valueName) {
+                $value[$valueName] = $value['this'];
+            }
+
+            if ($keyName) {
+                $value[$keyName] = $key;
+            }
+
+            $value['@index'] = $key;
+            $value['@key'] = $key;
+            $value['@first'] = $first;
+            $value['@last'] = $last;
+
+            $buffer[] = $options['fn']($value);
+        }
+
+        return implode('', $buffer);
+    }
+}
--- src/helpers.php
+++ src/helpers.php
@@ -1,71 +1,11 @@
 <?php
 declare(strict_types=1);
+
 return array(
     'if' => new \Eden\Handlebars\Helper\IfHelper(),
     'unless' => new \Eden\Handlebars\Helper\UnlessHelper(),
     'with' => new \Eden\Handlebars\Helper\WithHelper(),
-
-    'each' => function ($object, $options) {
-        $args = func_get_args();
-        $options = array_pop($args);
-
-        if (is_scalar($object) || !$object) {
-            return $options['inverse']();
-        }
-
-        //test foreach
-        $keyName = null;
-        $valueName = null;
-        //see handlebars.js {{#each array as |value, key|}}
-        if (strpos($options['args'], ' as |') !== false
-            && substr_count($options['args'], '|') === 2
-        ) {
-            list($tmp, $valueName) = explode('|', $options['args']);
-
-            if (strpos($valueName, ',') !== false) {
-                list($valueName, $keyName) = explode(',', trim($valueName));
-            }
-
-            $keyName = trim((string) $keyName);
-            $valueName = trim($valueName);
-        }
-
-        $buffer = array();
-        $object = (array) $object;
-
-        //get last
-        end($object);
-        $last = key($object);
-
-        //get first
-        reset($object);
-        $first = key($object);
-
-        foreach ($object as $key => $value) {
-            if (!is_array($value)) {
-                $value = array('this' => $value);
-            } else {
-                $value['this'] = $value;
-            }
-
-            if ($valueName) {
-                $value[$valueName] = $value['this'];
-            }
-
-            if ($keyName) {
-                $value[$keyName] = $key;
-            }
-
-            $value['@index'] = $key;
-            $value['@key'] = $key;
-            $value['@first'] = $first;
-            $value['@last'] = $last;
-
-            $buffer[] = $options['fn']($value);
-        }
-
-        return implode('', $buffer);
-    },
+    'each' => new \Eden\Handlebars\Helper\EachHelper(),
 
     'tokenize->' => function ($name) {
         //get args
